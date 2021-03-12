c8954bb39b8372f98fcc6b5ac28440a81a6f7505
Kotarosz727 <kotarosz727@gmail.com>
2021-03-12 13:15:12 +0900

    binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -14,6 +14,26 @@ def insert(node: Node, value: int) -> Node:
         node.right = insert(node.right, value)
     return node
 
+def inOrder(node: Node) -> None:
+    if node is not None:
+        inOrder(node.left)
+        print(node.value)
+        inOrder(node.right)
+    return    
+
+def search(node: Node, value) -> bool:
+    if node is None:
+        return False
+        
+    if value == node.value:
+        return True
+    elif value < node.value:
+        return search(node.left, value)
+    else:
+        return search(node.right, value)    
+                
+
+
 root = None
 root = insert(root, 3)
 root = insert(root, 6)        
@@ -22,4 +42,7 @@ root = insert(root, 1)
 root = insert(root, 2)        
 root = insert(root, 7)        
 root = insert(root, 8)        
-root = insert(root, 10)        
+root = insert(root, 10)   
+# inOrder(root)     
+res = search(root, 11)
+print(res)
