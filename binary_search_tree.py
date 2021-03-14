04471e394b792f8b2e6b657ba76a298ce7466603
Kotarosz727 <kotarosz727@gmail.com>
2021-03-14 14:27:17 +0900

    binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -24,25 +24,46 @@ def inOrder(node: Node) -> None:
 def search(node: Node, value) -> bool:
     if node is None:
         return False
-        
+
     if value == node.value:
         return True
     elif value < node.value:
         return search(node.left, value)
     else:
-        return search(node.right, value)    
-                
+        return search(node.right, value)
 
+def min_val(node: Node) -> None:
+    current = node
+    if current.left is None:
+        return current
+    return min_val(current.left)    
+
+def remove(node: Node, value:int) -> None:
+    if node is None:
+        return node
+
+    if value < node.value:
+        node.left = remove(node.left, value)
+    elif value > node.value:
+        node.right = remove(node.right, value)
+    else:
+        if node.left is None:
+            return node.right
+        elif node.right is None:
+            return node.left
+
+        temp = min_val(node.right)
+        node.value = temp.value
+        node.right = remove(node.right, temp.value)   
+    return node    
 
 root = None
 root = insert(root, 3)
-root = insert(root, 6)        
+root = insert(root, 6)
 root = insert(root, 5)        
+root = insert(root, 7)    
 root = insert(root, 1)        
-root = insert(root, 2)        
-root = insert(root, 7)        
-root = insert(root, 8)        
-root = insert(root, 10)   
-# inOrder(root)     
-res = search(root, 11)
-print(res)
+root = insert(root, 10)    
+root = insert(root, 2)      
+root = remove(root, 6)
+inOrder(root)
